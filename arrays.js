57c9ccaf80116ca180a699d0b8b5c4c3deba6b0b
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-03 10:49:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
 function addElementToBeginningOfArray (someArray, element ) {
   return [ element, ...someArray]
 }
+function destructivelyAddElementToBeginningOfArray (someArray, element ) {
+  someArray = [ element, ...someArray];
+  return someArray;
+}
